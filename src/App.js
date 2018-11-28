84c1fc599ea5ac44e65d32e1170bfbc6436d6dfb
fannyhasbi <fnyhsbi@gmail.com>
2018-11-28 22:06:34 +0700

    Add bus page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 // Screen Components
 import Home from './components/Home';
+import Buses from './components/Buses';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/buses">Bus</Link></li>
+          </ul>
+          
           <Route exact path="/"component={Home} />
+          <Route path="/buses" component={Buses} />
         </div>
       </BrowserRouter>
     );
