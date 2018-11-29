42dda4cfabe332593b40a4d5dea7a1ed56e0b4ca
fannyhasbi <fnyhsbi@gmail.com>
2018-11-30 05:34:24 +0700

    Give adddeparture functionality

--- src/components/departure/AddDeparture.js
+++ src/components/departure/AddDeparture.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import TimeInput from 'react-time-input';
 import Swal from 'sweetalert2';
+import qs from 'qs';
+import axios from 'axios';
+
+import BSR_APP from '../../config/constant';
 
 class AddDeparture extends React.Component {
   constructor(props){
@@ -9,34 +13,97 @@ class AddDeparture extends React.Component {
 
     this.state = {
       is_done: false,
+      list_bus: [],
+      list_tempat: [],
       id_perusahaan: 1,
-      perusahaan: 'Sinar Jaya',
-      asal: 'Semarang',
-      tujuan: 'Tegal',
-      berangkat: '15:00',
-      sampai: '18:00'
+      id_asal: 1,
+      id_tujuan: 1,
+      berangkat: '00:00',
+      sampai: '00:00'
     }
 
     this.handleSave = this.handleSave.bind(this);
-    this.handleBerangkat = this.handleBerangkat.bind(this);
-    this.handleSampai = this.handleSampai.bind(this);
   }
 
-  handleBerangkat(val){
-    this.setState({ berangkat: val });
-  }
+  componentDidMount(){
+    axios.get(BSR_APP.url + '/api/bus')
+    .then((response) => {
+      if(response.data.status === 200){
+        this.setState({
+          list_bus: response.data.data
+        });
+      }
+    })
+    .catch((error) => {
+      Swal("Oops", "Maaf, sedang terjadi kesalahan", "warning");
+      console.log(error);
+    });
 
-  handleSampai(val){
-    this.setState({ sampai: val });
+    axios.get(BSR_APP.url + '/api/place')
+    .then((response) => {
+      if(response.data.status === 200){
+        this.setState({
+          list_tempat: response.data.data
+        });
+      }
+    })
+    .catch((error) => {
+      Swal("Oops", "Maaf, sedang terjadi kesalahan", "warning");
+      console.log(error);
+    });
   }
 
   handleSave(e){
     e.preventDefault();
-    Swal({
-      title: "Berhasil menyimpan",
-      type: "success"
-    }).then((result) => {
-      this.setState({ is_done: true });
+
+    const {
+      id_perusahaan,
+      id_tujuan,
+      id_asal,
+      berangkat,
+      sampai } = this.state;
+
+    const postData = qs.stringify({
+      id_perusahaan,
+      id_tujuan,
+      id_asal,
+      berangkat,
+      sampai
+    });
+
+    axios.post(BSR_APP.url + '/api/add-departure',
+      postData,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+    )
+    .then((response) => {
+      if(response.data.status === 200){
+        Swal({
+          title: 'Berhasil',
+          text: 'Jadwal keberangkatan berhasil ditambahkan',
+          type: 'success',
+        }).then(() => {
+          this.setState({ is_done: true });
+        });
+      }
+      else {
+        Swal({
+          title: 'Oops',
+          text: 'Maaf, sedang terjadi kesalahan 1',
+          type: 'warning',
+        });
+      }
+    })
+    .catch((error) => {
+      Swal({
+        title: 'Oops',
+        text: 'Maaf, sedang terjadi kesalahan',
+        type: 'warning',
+      });
+      console.log('error', error);
     });
   }
 
@@ -52,28 +119,42 @@ class AddDeparture extends React.Component {
         <form onSubmit={this.handleSave} >
           <label htmlFor="perusahaan">Perusahaan</label><br/>
           <select onChange={(e) => this.setState({ id_perusahaan: e.target.value })}>
-            <option value="1">Sinar Jaya</option>
-            <option value="2">Nusantara</option>
-            <option value="3">Subur Jaya</option>
+            {
+              this.state.list_bus.map((el, i) => 
+                <option key={i} value={el.id}>{el.nama}</option>
+              )
+            }
           </select><br/>
 
           <label htmlFor="asal">Asal</label>
-          <input type="text" value={this.state.asal} onChange={(e) => this.setState({ asal: e.target.value})} /><br/>
+          <select onChange={(e) => this.setState({ id_asal: e.target.value })}>
+            {
+              this.state.list_tempat.map((el, i) => 
+                <option key={i} value={el.id}>{el.nama}</option>
+              )
+            }
+          </select><br/>
 
           <label htmlFor="tujuan">Tujuan</label>
-          <input type="text" value={this.state.tujuan} onChange={(e) => this.setState({ tujuan: e.target.value})} /><br/>
+          <select onChange={(e) => this.setState({ id_tujuan: e.target.value })}>
+            {
+              this.state.list_tempat.map((el, i) => 
+                <option key={i} value={el.id}>{el.nama}</option>
+              )
+            }
+          </select><br/>
 
           <label htmlFor="berangkat">Jam Berangkat</label>
           <TimeInput
             initTime={this.state.berangkat}
-            onTimeChange={this.handleBerangkat}
+            onTimeChange={(val) => this.setState({ berangkat: val })}
           />
           <br/>
 
           <label htmlFor="sampai">Jam Sampai</label>
           <TimeInput
             initTime={this.state.sampai}
-            onTimeChange={this.handleSampai}
+            onTimeChange={(val) => this.setState({ sampai: val })}
           />
           <br/>
 
