0b2f80cacd88471f7b8ba581ae51fa71465c8353
fannyhasbi <fnyhsbi@gmail.com>
2018-12-04 20:36:03 +0700

    Remove log

--- src/views/departure/Departure.js
+++ src/views/departure/Departure.js
@@ -31,7 +31,6 @@ class Departure extends React.Component {
   componentDidMount(){
     axios.get(BSR_APP.url + '/api/departure')
     .then((response) => {
-      console.log(response);
       if(response.data.status === 200){
         this.setState({
           departures: response.data.data
